707189ef28013ad1d653cc1dec2ae198c91f17e2
jiwoonghwang <hyae4924@gmail.com>
2022-06-18 17:46:58 +0900

    [CHORE] Fix env &config file set

--- server/modules/multer.js
+++ server/modules/multer.js
@@ -1,9 +1,18 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
-aws.config.loadFromPath(__dirname + '/../config/s3.json');
 
-const s3 = new aws.S3();
+
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID, //노출주의
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY, //노출주의
+  region: process.env.AWS_S3_REGION //노출주의
+});
+
+
+
+
 const upload = multer({
   storage: multerS3({
     s3: s3,
